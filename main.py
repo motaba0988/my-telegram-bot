16309e3db47d497148b9d618a5456d2bf03e787d
motaba0988 <aqataha.bayati112@gmail.com>
2025-07-26 15:24:42 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.environ["BOT_TOKEN"]
+TOKEN = os.environ["8266260672:AAHCXzXfsaE3gmkFs5g0-faR43o38_W30NQ"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ ربات با موفقیت راه افتاد!")
